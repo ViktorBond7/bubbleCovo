2dc9424cd713b2f786e872ba09d6664341314aab
Eduard Venidiktov <edven7777777@gmail.com>
2024-09-15 08:00:18 +0300

    second varian work slider don`t work puth!!

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,60 @@
+// import { defineConfig } from 'vite';
+// import glob from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+// import { globalStylesOptions } from './global.styles';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+
+//       rollupOptions: {
+//         input: glob.sync('./src/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     plugins: [
+//       injectHTML(),
+//       FullReload(['./src/**/**.html']),
+//       ViteImageOptimizer({
+//         exclude: /^sprite.svg$/,
+//         png: {
+//           quality: 60,
+//         },
+//         jpeg: {
+//           quality: 60,
+//         },
+//         jpg: {
+//           quality: 60,
+//         },
+//         webp: {
+//           quality: 60,
+//         },
+//       }),
+//     ],
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           additionalData: globalStylesOptions,
+//         },
+//       },
+//     },
+//   };
+// });
 import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
@@ -15,7 +72,7 @@ export default defineConfig(({ command }) => {
       sourcemap: true,
 
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync('./src/*.html'), // явна точка входу
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -26,6 +83,9 @@ export default defineConfig(({ command }) => {
         },
       },
       outDir: '../dist',
+    },
+    optimizeDeps: {
+      include: ['vite-plugin-html-inject', 'vite-plugin-full-reload'], // явний список залежностей для pre-bundling
     },
     plugins: [
       injectHTML(),
