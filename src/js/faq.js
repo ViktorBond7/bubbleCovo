438b49a619d91c2730822ae7e9a2821e75617050
Eduard Venidiktov <edven7777777@gmail.com>
2024-09-17 12:29:42 +0300

    add button plus&minus stiles ?

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,13 +3,18 @@ document.getElementById('toggleSvg').addEventListener('click', toggleText);
 function toggleText() {
   const svgText = document.getElementById('svgText');
   const extraText = document.getElementById('extraText');
+  const svgElement = document.getElementById('toggleSvg');
 
   if (svgText.innerHTML === '+') {
     svgText.innerHTML = '-';
     extraText.style.display = 'block';
+    svgElement.classList.remove('plus-style');
+    svgElement.classList.add('minus-style');
   } else {
     svgText.innerHTML = '+';
     extraText.style.display = 'none';
+    svgElement.classList.remove('minus-style');
+    svgElement.classList.add('plus-style');
   }
 }
 
